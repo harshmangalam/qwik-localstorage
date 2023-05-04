be063271f031a43d3d9d65f5d9ed1bd81fe02f8f
harshmangalam <harshdev8218@gmail.com>
2023-05-04 08:33:40 +0530

    added examples

---
+++ src/examples/user-list.tsx
@@ -0,0 +1,33 @@
+import { component$, useSignal } from "@builder.io/qwik";
+import { useLocalStorage } from "../components/use-localstorage";
+
+export default component$(() => {
+  const input = useSignal("");
+
+  const { data, set, remove } = useLocalStorage<string[]>("users", []);
+
+  return (
+    <div>
+      <input bind:value={input} type="text" placeholder="Enter username" />
+      <button
+        onClick$={() => {
+          set(
+            Array.isArray(data.value)
+              ? [...data.value, input.value]
+              : [input.value]
+          );
+          input.value = "";
+        }}
+      >
+        Add User
+      </button>
+
+      <ul>
+        {data.value?.map((user: any) => (
+          <li key={user}>{user}</li>
+        ))}
+      </ul>
+      <button onClick$={() => remove("users")}>Clear</button>
+    </div>
+  );
+});
